7257f774e59474199efa94049fd7fcfdba2add5f
Vini-Nice <viniciussantanapsn@gmail.com>
2024-11-22 14:07:05 -0300

    slza
    fernando

--- login.php
+++ login.php
@@ -301,6 +301,9 @@ text-align: center;
         <label for="senha">Senha:</label>
         <input type="password" id="senha" name="senha" required><br>
     </div>
+    <div class="naotemcad">
+        <p>Não tem cadastro? <a href="./cadastro.php">Cadastre-se</a></p>
+    </div>
     <button class="botao" type="submit">Entrar</button>
 </form>
 </div>
