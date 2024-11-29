27e8d97cc11082a135e5933f5fd369648dc96dd2
mebdrey <mebdelrey@gmail.com>
2024-11-29 15:48:27 -0300

    fsdfsd

--- login.php
+++ login.php
@@ -34,7 +34,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <title>Login - PKL Contacts</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="./css/login.css">
-    <link rel="stylesheet" href="testes.css">
    
 <script>
 // Efeito de digitação nos campos de entrada 
