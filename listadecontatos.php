4e8137092cd0b79de71f97d010184aafb2e648c2
mebdrey <mebdelrey@gmail.com>
2024-12-03 15:45:53 -0300

    fsdfs
    dh

--- listadecontatos.php
+++ listadecontatos.php
@@ -94,7 +94,7 @@ if (isset($_GET['excluir'])) {
     </table>
             <!--se não ouver contatos adicionados na conta-->
 <?php else: ?>
-    <p>Nenhum contato encontrado. Adicione um novo contato!</p>
+    <p class="mensagemerro">Nenhum contato encontrado. Adicione um novo contato!</p>
 <?php endif; ?>
 
 <!-- Rodape -->
